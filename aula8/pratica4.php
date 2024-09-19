0a7560d8d7f9d308264650ba0b331199baf6c737
Matheus Ferrari dos Santos <matheusferrarimf@unidavi.edu.br>
2024-09-19 20:47:56 -0300

    aaa

--- aula8/pratica4.php
+++ aula8/pratica4.php
@@ -4,7 +4,6 @@ $salario2= 2000;
 for ($salario1 = 1000; $salario1 < 1100; $salario1++) {
      
     if ($salario1 == 1050) {
-        echo "break na iteração de numero 50 <br>";
         break;  
     }  
     
