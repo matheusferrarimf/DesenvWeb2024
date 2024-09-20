390cedc6808c37ac79d141daa2bd282718a32e4d
Matheus Ferrari dos Santos <matheusferrarimf@unidavi.edu.br>
2024-09-19 21:25:36 -0300

    pratica 6

--- aula8/pratica6.php
+++ aula8/pratica6.php
@@ -1,10 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela Faculdade</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #000;
+            padding: 8px;
+            text-align: center;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
 
-$faculdade = array(array("matematica",5,8.5), array("portugues",2,9),
-array("Geografia",10,6), array("Educação fisica",2,8));
+    <table>
+        <tr>
+            <th>Disciplina</th>
+            <th>Faltas</th>
+            <th>Nota</th>
+        </tr>
+        <?php
+            $faculdade = array(
+                array("matematica",5,8.5), 
+                array("portugues",2,9),
+                array("Geografia",10,6), 
+                array("Educação fisica",2,8)
+            );
 
-echo $faculdade[0][0].":".$faculdade[0][1].":".$faculdade[0][2]."<br>";
-echo $faculdade[1][0].":".$faculdade[1][1].":".$faculdade[1][2].".<br>";
-echo $faculdade[2][0].":".$faculdade[2][1].":".$faculdade[2][2].".<br>";
-echo $faculdade[3][0].":".$faculdade[3][1].":".$faculdade[3][2].".<br>";
-?>
+            foreach ($faculdade as $materia) {
+                echo "<tr>";
+                echo "<td>".$materia[0]."</td>";
+                echo "<td>".$materia[1]."</td>";
+                echo "<td>".$materia[2]."</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+
+</body>
+</html>
