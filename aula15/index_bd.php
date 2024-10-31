e50474b416b1cf7678ba817ebf565e40c32c5296
Matheus Ferrari dos Santos <matheusferrarimf@unidavi.edu.br>
2024-10-31 20:29:58 -0300

    lab1

---
+++ aula15/index_bd.php
@@ -0,0 +1,15 @@
+<?php
+require_once "lib/conexaobd.php";
+
+$conexaoBd = new ConexaoBd();
+$conexaoBd->setHost('127.0.0.1');
+$conexaoBd->setPorta(5432);
+$conexaoBd->setDataBase("desenvweb");
+$conexaoBd->setUserName("postgres");
+$conexaoBd->setPassword("postgres");
+
+if(!$conexaoBd-> conectar()){
+    echo "falha ao conectar";
+};
+
+?>
---
+++ aula15/lib/conexaobd.php
@@ -0,0 +1,66 @@
+<?php
+class conexaoBd {
+        private $host;
+        private $porta;
+        private $userName;
+        private $password;
+        private $dbconn;
+        private $dataBase;
+        
+        public function setHost($host){
+            $this->host = ($host);
+
+        }
+        public function setPorta($porta){
+            $this->porta = ($porta);
+
+        }
+        public function setUserName($username){
+            $this->userName = ($username);
+
+        }
+        public function setPassword($password){
+            $this->password = ($password);
+
+        }
+
+        public function setDataBase($dataBase){
+            $this->dataBase = ($dataBase);
+
+        }
+
+        public function conectar() {
+           try  {
+            $this ->dbconn = pg_connect ($this -> getConnectionString());
+            if ($this->dbconn) {
+                return true;
+            }
+           } catch (Exception $e){
+                //
+           }
+        }
+
+        private function getConnectionString() {
+            return pg_connect("host=".$this->host.
+                               "port=".$this->porta. 
+                               "dbname=".$this->dataBase. 
+                               "user=".$this->userName.
+                               "password=".$this->password);
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
